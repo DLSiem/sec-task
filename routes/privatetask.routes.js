974bfb20c432cb3670b95a2a3d93e112c35f291c
DLSiem <pdlsiem17@gmail.com>
2024-07-01 23:35:32 +0530

    remove everything except README.md

--- routes/privatetask.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const privateTaskController = require("../controllers/privatetask.controller");
-
-router.post("/create/:userId", privateTaskController.create);
-router.get("/:userId", privateTaskController.getAll);
-
-module.exports = router;
